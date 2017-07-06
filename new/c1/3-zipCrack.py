6db489854e1f9193d6678f12382800fdd31235ea
yatin <iamyatin@gmail.com>
2017-07-06 18:15:59 +0530

    Update ZipCrack to take Option Arguments of File Names

--- new/c1/3-zipCrack.py
+++ new/c1/3-zipCrack.py
@@ -1,21 +1,34 @@
 import zipfile
 from threading import Thread
+import optparse
 
 
 def extract_file(zFile, password):
     try:
         zFile.extractall(pwd=bytes(password, encoding='utf-8'))
         print('[+] Password = ' + password + '\n')
-    except Exception as e:
-        # print("Tried:"+password+"\n")
-        # print(e)
+    except:
         pass
 
 
 def main():
-    z_file = zipfile.ZipFile('evil.zip')
-    print(type(z_file))
-    pass_file = open('dictionary.txt')
+    parser = optparse.OptionParser("usage%prog " + "-f <zip_file> -d "
+                                                   "<dictionary>")
+    parser.add_option('-f', dest='zname', type='string', help='specify zip '
+                                                               'file')
+    parser.add_option('-d', dest='dname', type='string', help='specify '
+                                                            'dictionary file')
+    (options, args) = parser.parse_args()
+
+    if (options.zname is None) | (options.dname is None):
+        print(parser.usage)
+        exit(0)
+    else:
+        zname = options.zname
+        dname = options.dname
+
+    z_file = zipfile.ZipFile(zname)
+    pass_file = open(dname)
     for line in pass_file:
         password = line.strip("\n")
         t = Thread(target=extract_file, args=(z_file, password))
